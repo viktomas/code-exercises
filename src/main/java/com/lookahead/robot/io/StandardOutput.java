e468204315e849ff9be2119120152f6aac92ecc4
Tomas Vik <vicek22@gmail.com>
2014-07-10 20:15:11 +1000

    StandardOutput alteration
    Using PrinterWriter instead of buffered writer. It provides println and doesn't produce any checked exception.

--- src/main/java/com/lookahead/robot/io/StandardOutput.java
+++ src/main/java/com/lookahead/robot/io/StandardOutput.java
@@ -1,10 +1,6 @@
 package com.lookahead.robot.io;
 
-import com.lookahead.robot.io.exception.OutputException;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 /**
  * Output writing to standard system output.
@@ -14,32 +10,23 @@ public class StandardOutput implements Output {
     /**
      * Writer writing to standard input.
      */
-    private BufferedWriter writer;
+    private PrintWriter writer;
 
     /**
      * Constructor initializes the writer.
      */
     public StandardOutput() {
-        this.writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        this.writer = new PrintWriter(System.out);
     }
 
     @Override
     public void writeLine(String line) {
-        try {
-            writer.write(line);
-            writer.newLine();
-        } catch (IOException e) {
-            throw new OutputException("Error during write to standard output", e);
-        }
-
+        writer.println(line);
+        writer.flush();
     }
 
     @Override
     public void close() {
-        try {
-            writer.close();
-        } catch (IOException e) {
-            throw new OutputException("Underlying writer couldn't be properly closed.",e);
-        }
+       writer.close();
     }
 }
