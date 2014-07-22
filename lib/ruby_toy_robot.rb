6c64b2cb33a9d95c4357b18e42c6d2147f7617c4
Tomas Vik <vicek22@gmail.com>
2014-07-22 21:04:22 +1000

    Executable
    wrapping all implemented functionality into ruby_robot executable script
    class App in RubyToyRobot module wrapping preparation and execution of
    app

--- lib/ruby_toy_robot.rb
+++ lib/ruby_toy_robot.rb
@@ -1,4 +1,37 @@
 Dir[File.dirname(__FILE__) + '/**/*.rb'].each {|file| require file }
+
+#main module of gem ruby_toy_robot
 module RubyToyRobot
-  # Your code goes here...
+
+  # App class holds static method for running whole application
+  class App
+
+    # method takes input and output as argument and runs robot application
+    def self.run(args,out=STDOUT)
+      robot = Robot.new
+      table = Table.new(4,4)
+      parser = Parser.new(robot,table,out)
+
+      input = input(args)
+      
+      while(line = input.gets)
+        #remove <CR> from end of the line
+        line = line.chomp
+        token = Lexer.parse_line(line)
+        command = parser.parse(token)
+        command.execute
+      end
+    end
+
+    # method optains input
+    # - file if it is specified
+    # - STDIN otherwise
+    def self.input(args)
+      if args.first
+        open(args.first)
+      else
+        STDIN
+      end
+    end
+  end
 end
